bf77b5a32a24c763bfe463f26132fa35a4c62088
Emerson S V <40496678+Dragoshotter14@users.noreply.github.com>
2022-07-16 12:59:24 -0300

    Update MainTranslate.java

--- src/MainTranslate.java
+++ src/MainTranslate.java
@@ -5,6 +5,7 @@ Github:https://github.com/Dragoshotter14*/
 
 import com.sun.source.util.SourcePositions;
 import java.net.SocketOption;
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -12,28 +13,28 @@ public class MainTranslate {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
         int bincode;
         int option;
         String wordcode;
 
-        System.out.println("Hi, this is my binary code translator developed in java, hope you like it");
+        System.out.println("Hi, this is my binary code translator developed in java, hope you like it.");
         System.out.print("\nYou want to convert word/number to binary code or binary code to number/word; please enter 1 or 2 proportionally:");
-        option = sc.nextInt();
-        sc.nextLine();
-        System.out.println(option);
-        if (option == 1) {
-            System.out.print("Please type your phrase/word here:");
-            wordcode = sc.nextLine();
-            System.out.println(wordcode);
-
+        try {
+            option = sc.nextInt();
+            sc.nextLine();
+            if (option == 1) {
+                System.out.print("Please type your phrase/word here:");
+                wordcode = sc.nextLine();
+            } else if (option == 2) {
+                System.out.print("Please enter your binary code here:");
+                bincode = sc.nextInt();
+            } else if (option >= 3) {
+                System.out.print("You have entered the wrong number, exiting.");
+            }
+            sc.close();
+        } catch (InputMismatchException e) {
+            System.out.print("Sorry your option number is too large, leaving");
         }
-        if (option == 2) {
-            System.out.print("\nPlease enter your binary code here:");
-            bincode = sc.nextInt();
-        }
-        else {
-            System.out.print("You have entered the wrong number, exiting.");
-        }
-        sc.close();
     }
 }
