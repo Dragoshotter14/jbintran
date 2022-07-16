929972baddd1af57d3d7d623725bc3c5303671eb
Emerson S V <40496678+Dragoshotter14@users.noreply.github.com>
2022-07-16 13:20:52 -0300

    Adding while function

--- src/MainTranslate.java
+++ src/MainTranslate.java
@@ -19,22 +19,27 @@ public class MainTranslate {
         String wordcode;
 
         System.out.println("Hi, this is my binary code translator developed in java, hope you like it.");
-        System.out.print("\nYou want to convert word/number to binary code or binary code to number/word; please enter 1 or 2 proportionally:");
-        try {
-            option = sc.nextInt();
-            sc.nextLine();
-            if (option == 1) {
-                System.out.print("Please type your phrase/word here:");
-                wordcode = sc.nextLine();
-            } else if (option == 2) {
-                System.out.print("Please enter your binary code here:");
-                bincode = sc.nextInt();
-            } else if (option >= 3) {
-                System.out.print("You have entered the wrong number, exiting.");
+        while (true) {
+            System.out.print("\nYou want to convert word/number to binary code or binary code to number/word; please enter 1 or 2 proportionally:");
+            try {
+                option = sc.nextInt();
+                sc.nextLine();
+                if (option == 1) {
+                    System.out.print("Please type your phrase/word here:");
+                    wordcode = sc.nextLine();
+
+                } else if (option == 2) {
+                    System.out.print("Please enter your binary code here:");
+                    bincode = sc.nextInt();
+                } else {
+                    System.out.print("You have entered the wrong number, exiting.");
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.print("Sorry your option number is too large, leaving");
+                break;
             }
-            sc.close();
-        } catch (InputMismatchException e) {
-            System.out.print("Sorry your option number is too large, leaving");
         }
     }
 }
+
